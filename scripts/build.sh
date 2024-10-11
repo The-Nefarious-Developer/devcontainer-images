62b47cd98121984453aa422a51838eaf935451da
NickChecan <nicholas.checan@icloud.com>
2024-10-10 23:16:42 -0500

    fix: location of the image content for the build

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,7 +8,7 @@ rm -rf "$TARGET_DIR"
 mkdir -p "$TARGET_DIR"
 
 # Copy content from the source directory to the build directory for further processing
-cp -R "$SRC_DIR"/../../src/"$TEMPLATE"/. "$TARGET_DIR"
+cp -R "$SRC_DIR"/../src/"$TEMPLATE"/. "$TARGET_DIR"
 
 # Simulate a --build-args behavior by replacing the image variant
 # with the version provided by github actions.
